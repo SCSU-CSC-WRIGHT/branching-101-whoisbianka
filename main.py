01d464d28a0139c63772f891f615010e2467829b
xjtrail-code <xjtrail@gmail.com>
2026-03-29 20:42:13 -0400

    Converted code in main.py to a function.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-total = 0
+def calculate_total():
+    total = 0
 
-for i in range(5):
-    number = int(input("Enter a number: "))
-    total += number
+    for i in range(5):
+        number = int(input("Enter a number: "))
+        total += number
 
-print("The running total is: ", total)
+    print("The running total is:", total)
+
+
+def main():
+    calculate_total()
+
+
+if __name__ == "__main__":
+    main()
