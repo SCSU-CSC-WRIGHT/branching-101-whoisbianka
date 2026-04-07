6a958fb6efed1ab3635f44248e7a4e58303f4505
xjtrail-code <xjtrail@gmail.com>
2026-04-07 16:16:00 -0400

    changes

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-"""There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
-total = 0
-
-for i in range(5):
-    number = int(input("Enter a number: "))
-    total += number
-    
-print("The running total is: ",  total)
-=======
 def calculate_total():
     total = 0
 
@@ -24,4 +14,3 @@ def main():
 
 if __name__ == "__main__":
     main()
->>>>>>> main
